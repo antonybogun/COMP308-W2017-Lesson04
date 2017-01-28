97491c5a8154bf96860dfe8440a4cdc5788ec701
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-01-28 16:27:17 -0500

    added front-end dependencies

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,6 @@ let currentDate = new Date();
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
-
-
   res.render('index', {
     title: 'Express',
     date: currentDate
@@ -17,9 +15,8 @@ router.get('/', (req, res, next) => {
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('index', {
-    title: 'About',
-    date: currentDate
+  res.render('about', {
+    title: 'About'
    });
 });
 
