3470722d9e9c7cb256f800ae565b13e29fb7c433
Tony Bogun <antonybogun@gmail.com>
2017-02-04 13:34:23 -0500

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -7,35 +7,35 @@ let currentDate = new Date();
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
-  res.render('index', {
+  res.render('content/index', {
     title: 'Home'
    });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('index', {
+  res.render('content/about', {
     title: 'About'
    });
 });
 
 /* GET projects page. */
 router.get('/projects', (req, res, next) => {
-  res.render('index', {
+  res.render('content/projects', {
     title: 'Projects'
    });
 });
 
 /* GET services page. */
 router.get('/services', (req, res, next) => {
-  res.render('index', {
+  res.render('content/services', {
     title: 'Services'
    });
 });
 
 /* GET contact page. */
 router.get('/contact', (req, res, next) => {
-  res.render('index', {
+  res.render('content/contact', {
     title: 'Contact'
    });
 });
